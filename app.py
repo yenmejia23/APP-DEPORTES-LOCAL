5171a0d75e7c7722465459e06f0e67f729916a48
yenmejia23 <yenmejia.23@gmail.com>
2026-06-15 22:40:40 -0600

    Arreglo de puerto

--- app.py
+++ app.py
@@ -28,4 +28,6 @@ def registrar_equipo():
     return redirect(url_for('inicio'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
